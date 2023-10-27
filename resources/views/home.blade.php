f0514dc7ea86636a660527a047aacb3008532abf
Pietro907 <pietrop.puri@gmail.com>
2023-10-27 17:34:31 +0200

    add db miss ultimate miles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,33 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel-Model-Controller</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        
-       
-    </head>
-    <body class="antialiased">
-        
-        <h1>
-            Welcome
-        </h1>
+    <title>Laravel-Model-Controller</title>
 
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-    </body>
+    <!-- style -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body class="antialiased">
+
+    <header>
+        @include('layout.header')
+    </header>
+
+    
+    <h2>Ciao</h2>
+
+    <main>
+        @include('layout.main')
+    </main>
+</body>
+
 </html>
---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,17 @@
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-2">
+            <div class="card">
+
+                <h6>Title: </h6>
+                <p>1</p>
+                <p>2</p>
+                <p>3</p>
+                <p>4</p>
+                
+            </div>
+        </div>
+    </div>
+</div>
